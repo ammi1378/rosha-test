46beb406cef3e2e1d35ae0f0de7f76592778be87
ammi1378 <ammi1378@gmail.com>
2022-11-16 19:17:36 +0330

    Fix url

--- src/rosha-api/base.ts
+++ src/rosha-api/base.ts
@@ -22,8 +22,8 @@ import globalAxios, {
   AxiosRequestConfig,
 } from "axios";
 
-export const BASE_URL = "https://rosha.adiaweb.io".replace(/\/+$/, "");
-export const BASE_PATH = "https://rosha.adiaweb.io/api".replace(/\/+$/, "");
+export const BASE_URL = "http://94.101.179.129:8082".replace(/\/+$/, "");
+export const BASE_PATH = "http://94.101.179.129:8082/api".replace(/\/+$/, "");
 
 /**
  *
